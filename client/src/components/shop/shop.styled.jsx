30f4f1efafd8b7dd5f7d0d33c0e3abec37d94f72
Marcelo Távora <marceloccunifor@gmail.com>
2022-04-24 17:56:01 -0300

    hotfix shop

--- client/src/components/shop/shop.styled.jsx
+++ client/src/components/shop/shop.styled.jsx
@@ -2,9 +2,16 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   border-radius: 14px;
-  padding: 30px;
+
   box-shadow: 0px 0px 8px #eee;
   height: calc(100vh - 220px);
+  margin: 30px;
+  padding: 30px;
+  overflow: hidden;
+  @media screen and (max-width: 700px) {
+    & {
+    }
+  }
 `;
 export const AmountBox = styled.div`
   @media screen and (max-width: 700px) {
@@ -28,5 +35,5 @@ export const ItemsBox = styled.div`
   margin-top: 50px;
   height: 400px;
   overflow: auto;
-  padding: 30px;
+  padding-bottom: 50px;
 `;
